fcfb4eab716e82489c5e03ed34b3d7cd36b9d3ee
AntjorZ <antonredmicrosoft@gmail.com>
2026-04-06 18:52:24 +0300

    Add files via upload

--- dart_application_1.dart
+++ dart_application_1.dart
@@ -1,136 +1,53 @@
+import 'dart:io';
 import 'dart:math';
+import 'dart:convert';
+import 'package:characters/characters.dart';
+
+enum Mood {
+  happy('\u{1F60A}', 'счастливое', 8),
+  sad('\u{1F622}', 'грустное', 3),
+  cool('\u{1F60E}', 'крутое', 9);
+
+  final String emoji;
+  final String description;
+  final int energy;
+
+  const Mood(this.emoji, this.description, this.energy);
+}
 
 void main() {
-  // Список студентов
-  List<String> students = [
-    'Иван',
-    'Мария',
-    'Алексей',
-    'Ольга',
-    'Дмитрий',
-    'Екатерина'
-  ];
+  stdout.encoding = utf8;
 
-  // Список предметов
-  List<String> subjects = [
-    'Математика',
-    'Физика',
-    'Информатика',
-    'История'
-  ];
+  stdout.write('Введите имя: ');
+  String name = stdin.readLineSync() ?? 'Гость';
 
-  // Оценки студентов (каждый студент -> список оценок по предметам)
-  List<List<int>> grades = [
-    [5, 4, 5, 3],
-    [4, 5, 4, 4],
-    [3, 4, 5, 2],
-    [5, 5, 5, 5],
-    [4, 3, 4, 3],
-    [5, 4, 4, 4]
-  ];
+  print('\nПривет, $name!');
+  
+  var random = Random();
+  var mood = Mood.values[random.nextInt(Mood.values.length)];
+  
+  print('Твоё настроение: ${mood.emoji} ${mood.description} (${mood.energy}/10)');
 
-  print('СПИСОК СТУДЕНТОВ');
-  for (int i = 0; i < students.length; i++) {
-    print('${i + 1}. ${students[i]}');
-  }
 
-  print('\nСПИСОК ПРЕДМЕТОВ');
-  for (int i = 0; i < subjects.length; i++) {
-    print('${i + 1}. ${subjects[i]}');
-  }
+  // Unicode эмодзи
+  int code = mood.emoji.runes.first;
+  print('Юникод: U+${code.toRadixString(16).toUpperCase()}');
 
-  print('\nОЦЕНКИ СТУДЕНТОВ');
-  for (int i = 0; i < students.length; i++) {
-    for (int j = 0; j < subjects.length; j++) {
-      print('${students[i]} — ${subjects[j]}: ${grades[i][j]}');
-    }
-  }
+  // анализ
+  stdout.write('\nАнализировать эмодзи? (yes/no): ');
+  String answer = stdin.readLineSync() ?? '';
 
-  print('\nСРЕДНИЙ БАЛЛ ПО КАЖДОМУ ПРЕДМЕТУ');
-  for (int j = 0; j < subjects.length; j++) {
-    double sum = 0;
-    for (int i = 0; i < students.length; i++) {
-      sum += grades[i][j];
-    }
-    double avg = sum / students.length;
-    print('${subjects[j]}: ${avg.toStringAsFixed(2)}');
-  }
+  if (answer.toLowerCase() == 'yes') {
+    stdout.write('Введи эмодзи: ');
+    String input = stdin.readLineSync() ?? '';
 
-  print('\nСРЕДНИЙ БАЛЛ КАЖДОГО СТУДЕНТА');
+    print('\nДлина строки: ${input.length}');
+    print('Кодовых точек: ${input.runes.length}');
+    print('Реальных символов: ${input.characters.length}');
 
-  double bestAvg = 0;
-  String bestStudent = '';
-
-  for (int i = 0; i < students.length; i++) {
-    double sum = grades[i].reduce((a, b) => a + b).toDouble();
-    double avg = sum / subjects.length;
-
-    print('${students[i]}: ${avg.toStringAsFixed(2)}');
-
-    if (avg > bestAvg) {
-      bestAvg = avg;
-      bestStudent = students[i];
-    }
-  }
-
-  print('\nЛУЧШИЙ СТУДЕНТ');
-  print('$bestStudent со средним баллом ${bestAvg.toStringAsFixed(2)}');
-
-  print('\nПРЕДМЕТ С НАИМЕНЬШИМ СРЕДНИМ БАЛЛОМ');
-
-  double minAvg = double.infinity;
-  String worstSubject = '';
-
-  for (int j = 0; j < subjects.length; j++) {
-    double sum = 0;
-    for (int i = 0; i < students.length; i++) {
-      sum += grades[i][j];
-    }
-
-    double avg = sum / students.length;
-
-    if (avg < minAvg) {
-      minAvg = avg;
-      worstSubject = subjects[j];
-    }
-  }
-
-  print('$worstSubject: ${minAvg.toStringAsFixed(2)}');
-
-  print('\nОБЩИЙ СРЕДНИЙ БАЛЛ ПО ГРУППЕ');
-
-  int total = 0;
-  int count = 0;
-
-  for (var studentGrades in grades) {
-    for (var g in studentGrades) {
-      total += g;
-      count++;
-    }
-  }
-
-  double groupAvg = total / count;
-  print(groupAvg.toStringAsFixed(2));
-
-  print('\nПРЕДМЕТЫ БЕЗ ПОВТОРОВ');
-  Set<String> uniqueSubjects = subjects.toSet();
-  uniqueSubjects.forEach(print);
-  print('Количество предметов: ${uniqueSubjects.length}');
-
-  print('\nСТУДЕНТЫ БЕЗ ОЦЕНКИ 2');
-
-  for (int i = 0; i < students.length; i++) {
-    if (!grades[i].contains(2)) {
-      print(students[i]);
-    }
-  }
-
-  print('\nСТУДЕНТЫ С ОЦЕНКАМИ НЕ НИЖЕ 4');
-
-  for (int i = 0; i < students.length; i++) {
-    bool allGood = grades[i].every((g) => g >= 4);
-    if (allGood) {
-      print(students[i]);
+    print('\nЮникод каждого:');
+    for (var rune in input.runes) {
+      print('U+${rune.toRadixString(16).toUpperCase()}');
     }
   }
 }
